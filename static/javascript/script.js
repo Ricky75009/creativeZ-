8fe579e9089d224342bee87372f275af1ff1e3e9
“[Roman <[roman.kindler@code.berlin]>
2020-03-26 17:48:36 +0100

    try a bit to work with javascript

---
+++ static/javascript/script.js
@@ -0,0 +1,32 @@
+function pot_selector(){
+    console.log("This in progress");
+    
+    var div = document.createElement('div');
+    div.textContent = " ";
+    div.setAttribute('id', 'sidebar');
+    document.body.appendChild(div);
+
+
+
+    }
+
+function design_selector(){
+    var div = document.createElement('div');
+    div.textContent = " ";
+    div.setAttribute('id', 'sidebar');
+    document.body.appendChild(div);
+    }
+
+function main_color_selector(){
+    var div = document.createElement('div');
+    div.textContent = " ";
+    div.setAttribute('id', 'sidebar');
+    document.body.appendChild(div);
+    }
+
+function secondary_color_selector(){
+    var div = document.createElement('div');
+    div.textContent = " ";
+    div.setAttribute('id', 'sidebar');
+    document.body.appendChild(div);
+    }
