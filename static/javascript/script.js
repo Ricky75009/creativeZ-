844cc808f4c19ee59d7ec9d5cef5ad5608a26ec6
“[Roman <[roman.kindler@code.berlin]>
2020-03-30 12:30:26 +0200

    added some javscript to prepare backend

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,3 +1,9 @@
+let pot='';
+let design='';
+let main_color='';
+let secondary_color='';
+
+
 function pot_selector() {
     $('.pot-selector').toggleClass('show');
 
@@ -13,5 +19,37 @@ function main_color_selector() {
 
 function secondary_color_selector() {
     $('.secondary_color-selector').toggleClass('show');
+}
+
+function change_pot(pot_temp) {
+    $('.pot-selector').toggleClass('show');
+    console.log(`pot changed to ${pot_temp}`);
+    pot=pot_temp;
+}
+
+function change_desing(design_temp){
+    $('.design-selector').toggleClass('show');
+    console.log(`design changed to ${design_temp}`);
+    design=design_temp;
+}
+
+function change_main_color(main_color_temp){
+    $('.main_color-selector').toggleClass('show');
+    console.log(`main color changed to ${main_color_temp}`);
+    main_color=main_color_temp;
+}
+
+function change_secondary_color(secondary_color_temp){
+    $('.secondary_color-selector').toggleClass('show');
+    console.log(`secondary color changed to ${secondary_color_temp}`);
+    secondary_color=secondary_color_temp;
+}
+
+function back(tab){
+    $('.'+tab).toggleClass('show');
+}
+
+function render(){
+
 }
 
