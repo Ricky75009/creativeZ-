f0dcdd5f24ec888ee98d096f87858dd314fbc839
“[Roman <[roman.kindler@code.berlin]>
2020-04-01 19:16:02 +0200

    share option finished!

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -3,6 +3,23 @@ let design='';
 let main_color='';
 let secondary_color='';
 
+$('#hoverlink').hover(function(){
+    $('.hover_text_link').toggleClass('show');
+    }, function(){
+    $('.hover_text_link').toggleClass('show');
+  });
+
+$('#hovertwitter').hover(function(){
+    $('.hover_text_twitter').toggleClass('show');
+    }, function(){
+    $('.hover_text_twitter').toggleClass('show');
+  });
+
+$('#hoverfb').hover(function(){
+    $('.hover_text_fb').toggleClass('show');
+    }, function(){
+    $('.hover_text_fb').toggleClass('show');
+  });
 
 function pot_selector() {
     $('.pot-selector').toggleClass('show');
@@ -56,9 +73,12 @@ function render(){
 function share(){
     
     $('.facebook').toggleClass('show');
-   
     $('.twitter').toggleClass('show');
     $('.link').toggleClass('show');
+    $('.plink').toggleClass('show');
+    $('.ptwitter').toggleClass('show');
+    $('.pfb').toggleClass('show');
+
    
     $('#arrow').toggleClass('show');
     $('#arrow_2').toggleClass('show');
@@ -74,47 +94,58 @@ function share(){
 
 
 function facebook(){
-    // window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fcatalogo.kindler.design%2F&amp%3Bsrc=sdkpreparse','_blank','height=400,width=400');
+     window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fcatalogo.kindler.design%2F&amp%3Bsrc=sdkpreparse','_blank','height=400,width=400');
 
 
     $('.link').toggleClass('show');
     $('.twitter').toggleClass('show');
     $('.facebook').toggleClass('show');
-
+    
+    $('#arrow').toggleClass('show');
+    $('#arrow_2').toggleClass('show');
+    $('.small_lines').toggleClass('show');
+    $('#cross').toggleClass('show');
+    $('#cross_2').toggleClass('show');
+    $('#vertical_line').toggleClass('show');
+    $('#horizontal_line').toggleClass('show');
 }
 
 function twitter(){
-    //window.open('https://twitter.com/intent/tweet?text=Look%20at%20whaaaaaat%20I%20did%20on%20http://catalogo.kindler.design%20!','_blank','height=400,width=400');
+    window.open('https://twitter.com/intent/tweet?text=Look%20at%20whaaaaaat%20I%20did%20on%20http://catalogo.kindler.design%20!','_blank','height=400,width=400');
     
     $('.facebook').toggleClass('show');
-    
-   
     $('.twitter').toggleClass('show');
-    
     $('.link').toggleClass('show');
-
-
+    
+    $('#arrow').toggleClass('show');
+    $('#arrow_2').toggleClass('show');
+    $('.small_lines').toggleClass('show');
+    $('#cross').toggleClass('show');
+    $('#cross_2').toggleClass('show');
+    $('#vertical_line').toggleClass('show');
+    $('#horizontal_line').toggleClass('show');
 }
 
 function link(){
 let test = document.createElement('input');
-
 test.setAttribute('Value',document.getElementById('test').innerHTML);
 document.body.appendChild(test);
 test.select();
 document.execCommand('copy');
 document.body.removeChild(test);
 
-
-
 $('.facebook').toggleClass('show');
-    
-   
 $('.twitter').toggleClass('show');
-
 $('.link').toggleClass('show');
 
-
-
+$('#arrow').toggleClass('show');
+$('#arrow_2').toggleClass('show');
+$('.small_lines').toggleClass('show');
+$('#cross').toggleClass('show');
+$('#cross_2').toggleClass('show');
+$('#vertical_line').toggleClass('show');
+$('#horizontal_line').toggleClass('show');
+$('.hover_text_link.show').removeClass('plus');
 }
+
 
