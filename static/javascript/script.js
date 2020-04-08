06d3419880f5417e30fa0e0e2ddbc4e285a302d5
“[Roman <[roman.kindler@code.berlin]>
2020-04-08 12:00:28 +0200

    small fixes

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -2,7 +2,7 @@ let pot='chico angular';
 let design='color sólido';
 let main_color='negro';
 let secondary_color='';
-alert('on est lààà')
+
 
 $('#hoverlink').hover(function(){
     $('.hover_text_link').toggleClass('show');
@@ -100,7 +100,11 @@ function main_color_selector() {
 
                let dot =document.createElement('div');
                dot.setAttribute('id','dot');
-               dot.setAttribute('style','background-color:#'+list_hex[i]);
+               if(list_hex[i]=='ffffff'){
+                dot.setAttribute('style','background-color:#'+list_hex[i]+';border: 2px solid black;')
+            }else{
+                dot.setAttribute('style','background-color:#'+list_hex[i]);
+            }
                
                
                 let paragraph = document.createElement('p');
@@ -122,26 +126,51 @@ function main_color_selector() {
 function secondary_color_selector() {
     $('.secondary_color-selector').toggleClass('show');
     $('#secondary_color_options').empty();
+
+    let list_shex
+    let xhr_shex = new XMLHttpRequest();
+    xhr_shex.responseType = 'json';
+    xhr_shex.open("GET", "http://127.0.0.1:5000/api/secondary_color_hex");
+    xhr_shex.onload= function(){
+    list_shex = xhr_shex.response;}
+    xhr_shex.send();
+    
         let xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
         xhr.open("GET", "http://127.0.0.1:5000/api/secondary_color");
         xhr.onload = function () {
            let list_secondary_color = xhr.response;
            for (let i = 0; i < list_secondary_color.length; i++){
+            let div=document.createElement('div');
+            div.setAttribute('id',list_secondary_color[i]);
+            div.setAttribute('display','inline-block');
+            document.getElementById('secondary_color_options').appendChild(div);
+
+            let dot =document.createElement('div');
+            dot.setAttribute('id','dot');
+            if(list_shex[i]=='ffffff'){
+                dot.setAttribute('style','background-color:#'+list_shex[i]+';border: 2px solid black;')
+            }else{
+                dot.setAttribute('style','background-color:#'+list_shex[i]);
+            }
                 let paragraph = document.createElement('p');
                 paragraph.textContent = list_secondary_color[i];
-              paragraph.setAttribute("onclick", "change_secondary_color('"+list_secondary_color[i]+"')");
-              document.getElementById('secondary_color_options').appendChild(paragraph);
+                paragraph.setAttribute("onclick", "change_secondary_color('"+list_secondary_color[i]+"')");
+                paragraph.setAttribute('float','left');
+                paragraph.setAttribute('style','margin-left:200px');
+                $('#'+list_secondary_color[i]).append(dot,paragraph);
             }
         }
          xhr.send();
-
+        alert(list_secondary_color)
 }
 
 function change_pot(pot_temp) {
     $('.pot-selector').toggleClass('show');
     console.log(`pot changed to ${pot_temp}`);
     pot=pot_temp;
+    main_color='negro';
+    secondary_color='';
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", 'http://127.0.0.1:5000/api/change_pot');
@@ -150,19 +179,22 @@ function change_pot(pot_temp) {
 
     render(pot,design,main_color,secondary_color);
 
+
 }
 
 function change_design(design_temp){
     $('.design-selector').toggleClass('show');
     console.log(`design changed to ${design_temp}`);
     design=design_temp;
-
+    main_color='negro';
+    secondary_color='';
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", 'http://127.0.0.1:5000/api/change_design');
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify({ 'design': design }));
     render(pot,design,main_color,secondary_color);
+
 }
 
 function change_main_color(main_color_temp){
